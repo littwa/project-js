17cf95ae6377482478822e824b8b78073f3490ac
littwa <littwa777@gmail.com>
2020-07-07 02:22:39 +0300

    start project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles.css';
+
+
+// inputValue - имя переменной для значия input в форме поиска(в хедере) 
+// watched, queue - названия ключей для записи(и чтения) данных с localStorage
+// pageNumber - активной страницы
